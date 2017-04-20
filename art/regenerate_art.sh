18d4fb2d8e417d7eafb6a4591fc0b8a7e101230c
idk <problemsolver@openmailbox.org>
2017-04-19 22:40:13 -0400

    new webpage 'lair signing key' moving to better art regeneration 170419224012

--- art/regenerate_art.sh
+++ art/regenerate_art.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env /bin/sh
 for file in $(find /usr/share/digitalandy/skel/ -name *.txt); do
-        if [ $file != "colors.txt" ]; then
-                echo $file
+        if [ $file != "/usr/share/digitalandy/skel/colors.txt" ]; then
+                echo $file | sed 's|/usr/share/digitalandy/skel/||g'
                 # dandy -incl=/usr/share/digitalandy/skel/colors.txt \
                 #   -desc="$file" \
-                #   -dir
-                #
+                #   -dir=
+                #   -
         fi
 done
